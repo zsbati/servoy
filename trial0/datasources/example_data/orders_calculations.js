0c52960a6705e3c2d5a1b931e20d144ccb0357ba
zsbati <zbati123@gmail.com>
2024-07-26 23:08:54 +0100

    Chapter 4 ready, Dg!

--- trial0/datasources/example_data/orders_calculations.js
+++ trial0/datasources/example_data/orders_calculations.js
@@ -3,5 +3,10 @@
  */
 function order_total()
 {
-	return 1;
+	var sum = 0;
+	for (var i=1; i<= orders_to_order_details.getSize(); i++){
+		var record = orders_to_order_details.getRecord(i);
+		sum += record.subtotal;
+	}
+	return sum;
 }
